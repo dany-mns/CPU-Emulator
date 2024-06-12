3c886804a4f3ed387c8c4e857b1d9f83b013f6a8
danym <dannymanastireanu@gmail.com>
2024-06-12 09:37:19 +0000

    Mem and cpu

--- main.cpp
+++ main.cpp
@@ -1,31 +1,56 @@
 #include <stdio.h>
+#include <cstdint>
 
-struct CPU {
-    using Byte = unsigned char;
-    using Word = unsigned short;
+#define MAX_MEMORY 64 * 1024 // 64 Kb
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
+struct Memory
+{
+    Byte data[MAX_MEMORY];
+
+    void init()
+    {
+        for (uint32_t i = 0; i < MAX_MEMORY; i++)
+        {
+            data[i] = 0;
+        }
+    }
+};
+
+struct CPU
+{
     Word program_counter;
     Byte stack_pointer;
 
     Byte A, X, Y;
 
     // status flags
-    Byte C: 1;
-    Byte Z: 1;
-    Byte I: 1;
-    Byte D: 1;
-    Byte B: 1;
-    Byte V: 1;
-    Byte N: 1;
+    Byte carry_flag : 1;
+    Byte zero_flag : 1;
+    Byte interrupt_disable_flag : 1;
+    Byte decimal_flag : 1;
+    Byte break_flag : 1;
+    Byte overflow_flag : 1;
+    Byte negative_flag : 1;
 
-    void reset() {
-        program_counter = 0xFFC;
-        D = 0;
+    void reset(Memory &memory)
+    {
+        program_counter = 0xFFFC;
+        stack_pointer = 0x0100;
+        decimal_flag = 0;
+        A = X = Y = 0;
+        carry_flag = zero_flag = interrupt_disable_flag = decimal_flag = break_flag = overflow_flag = negative_flag = 0;
+        memory.init();
     }
 };
 
-int main() {
+int main()
+{
+    Memory memory;
     CPU cpu;
+    cpu.reset(memory);
     printf("hello\n");
     return 0;
 }
