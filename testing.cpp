8016aa572ca4562201f692bb1e4f818dacdad8dc
danym <dannymanastireanu@gmail.com>
2024-06-26 16:54:39 +0000

    Dummy interview test

---
+++ testing.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <iostream>
+
+template<typename K, typename V>
+class interval_map {
+	friend void IntervalMapTest();
+	V m_valBegin;
+	std::map<K,V> m_map;
+public:
+	// constructor associates whole range of K with val
+	interval_map(V const& val)
+	: m_valBegin(val)
+	{}
+
+	// Assign value val to interval [keyBegin, keyEnd).
+	// Overwrite previous values in this interval.
+	// Conforming to the C++ Standard Library conventions, the interval
+	// includes keyBegin, but excludes keyEnd.
+	// If !( keyBegin < keyEnd ), this designates an empty interval,
+	// and assign must do nothing.
+	void assign( K const& keyBegin, K const& keyEnd, V const& val ) {
+// INSERT YOUR SOLUTION HERE
+	}
+
+	// look-up of the value associated with key
+	V const& operator[]( K const& key ) const {
+		auto it=m_map.upper_bound(key);
+		if(it==m_map.begin()) {
+			return m_valBegin;
+		} else {
+			return (--it)->second;
+		}
+	}
+};
+
+int main()
+{
+    std::cout << "" << std::endl;
+    int a = 123;
+    return 0;
+}
